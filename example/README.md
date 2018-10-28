991e9b4c42c8aead76a5817b11112013dbb763dd
golery <noreply@mail.com>
2018-10-28 17:27:30 -0400

    Initial commit

---
+++ example/README.md
@@ -0,0 +1,12 @@
+An example of how to use golery-editor
+
+#HOW TO USE
+Update the code, update version
+npm publish
+
+In example
+yarn upgrade golery editor
+yarn run dev
+Access at localhost:8080
+
+
---
+++ example/webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require("webpack");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
